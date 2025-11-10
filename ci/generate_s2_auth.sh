9080ab840cde047dfb3baa3895df56d786e3d9a2
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2025-11-10 16:31:47 +0100

    Working example of code gen

--- ci/generate_s2_auth.sh
+++ ci/generate_s2_auth.sh
@@ -2,4 +2,6 @@
 
 
 . .venv/bin/activate
-datamodel-codegen  --input ./specification/s2-over-ip-common.yml --input-file-type openapi --output-model-type pydantic_v2.BaseModel --output src/s2-auth/gen_protocol/gen_s2_auth.py --use-one-literal-as-default --use-subclass-enum --output-datetime-class AwareDatetime --use-double-quotes
+# datamodel-codegen  --input ./specification/s2-over-ip-connection-init.yml --input-file-type openapi --output-model-type pydantic_v2.BaseModel --output src/s2auth/gen_protocol/connection_init.py --use-one-literal-as-default --use-subclass-enum --output-datetime-class AwareDatetime --use-double-quotes
+cd ./specification
+fastapi-codegen --input s2-over-ip-connection-init.yml --input s2-over-ip-pairing.yml --output-model-type pydantic_v2.BaseModel --output ../src/s2auth/gen_protocol/
--- ci/update_dependencies.sh
+++ ci/update_dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 . .venv/bin/activate
-pip-compile --extra=ws --extra=development --extra=docs --extra=testing --output-file=./dev-requirements.txt pyproject.toml
+pip-compile -U --extra=ws --extra=development --extra=docs --extra=testing --output-file=./dev-requirements.txt pyproject.toml
