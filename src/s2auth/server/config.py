27731391621a7ec5dc037b4b19ce87b894598c09
Dolf Andringa <dolfandringa@gmail.com>
2026-02-04 13:39:20 -0500

    First dependency injection code

---
+++ src/s2auth/server/config.py
@@ -0,0 +1,17 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from s2auth.server.dependencies import register_provider
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=[".env", ".env.docker"], extra="ignore", env_nested_delimiter="__"
+    )
+    sqlalchemy_db_uri: SecretStr = SecretStr(
+        "postgresql://postgres:postgres@localhost/s2auth"
+    )
+
+
+@register_provider()
+async def config() -> Config:
+    return Config()
