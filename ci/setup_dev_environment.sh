aabad9d4b7a3bfd80cf1520c53e3ca3557d5fc31
Jorrit Nutma <jorrit.nutma@tno.nl>
2026-02-05 16:00:37 +0100

    Check which python executable is present

--- ci/setup_dev_environment.sh
+++ ci/setup_dev_environment.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 CUR_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
+if command -v python3 >/dev/null 2>&1; then
+  PY=python3
+elif command -v python >/dev/null 2>&1; then
+  PY=python
+else
+  echo "Error: Neither python3 nor python found in PATH." >&2
+  exit 1
+fi
+
+echo $PY
+
 if ! command -v pyenv >/dev/null 2>&1; then
     echo "Please install pyenv: https://github.com/pyenv/pyenv?tab=readme-ov-file#a-getting-pyenv"
 fi
@@ -10,7 +21,7 @@ if command -v pipx >/dev/null 2>&1; then
     fi
     pipx upgrade poetry
 else
-    curl -sSL https://install.python-poetry.org | python -
+    curl -sSL https://install.python-poetry.org | $PY -
 fi
 
 ./$CUR_DIR/install_dependencies.sh
